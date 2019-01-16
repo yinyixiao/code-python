56aad56553780c6d22a8e958c90fe695e7d90d0f
yinyixiao <996447988@qq.com>
2019-01-16 11:43:24 +0800

    '闭包'

--- leetcode/hello.py
+++ leetcode/hello.py
@@ -13,9 +13,24 @@ def test():
     else:
         print('Too many arguments!')
 
-if __name__=='__main__':
-    test()
+# if __name__=='__main__':
+# #     test()
+# #
+# # li = [lambda :x for x in range(10)]
+# # print(type(li))
+# # print(type(li[0]))
 
-li = [lambda :x for x in range(10)]
-print(type(li))
-print(type(li[0]))
+def createCounter():
+    x = [0]
+    def counter():
+        x[0] += 1
+        return x[0]
+    return counter
+
+def createCounter():
+    n = 0
+    def counter():
+        nonlocal n
+        n += 1
+        return n
+    return counter
