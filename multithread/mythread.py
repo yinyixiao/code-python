c34e2696cf75a1cc10b05db03101d1a51dc18e21
yinyixiao <996447988@qq.com>
2019-01-16 15:47:53 +0800

    '多线程'

---
+++ multithread/mythread.py
@@ -0,0 +1,18 @@
+import time, threading
+
+# 新线程执行的代码:
+#启动一个线程就是把一个函数传入并创建Thread实例，然后调用start()开始执行：
+def loop():
+    print('thread %s is running...' % threading.current_thread().name)
+    n = 0
+    while n < 5:
+        n = n + 1
+        print('thread %s >>> %s' % (threading.current_thread().name, n))
+        time.sleep(1)
+    print('thread %s ended.' % threading.current_thread().name)
+
+print('thread %s is running...' % threading.current_thread().name)
+t = threading.Thread(target=loop, name='LoopThread')
+t.start()
+t.join()
+print('thread %s ended.' % threading.current_thread().name)
